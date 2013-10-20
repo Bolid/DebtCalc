d527025b36214bc1a9f8f828100cb5f82247adba
Bolid <dmitriy.grigorev@gmail.com>
2013-10-21 00:55:29 +0400

    Обновление представления карточек и добавление тени карточке общего платежа

--- src/ru/omdroid/DebtCalc/Forms/InfoDebt.java
+++ src/ru/omdroid/DebtCalc/Forms/InfoDebt.java
@@ -4,10 +4,10 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.*;
 import ru.omdroid.DebtCalc.AppData;
@@ -21,6 +21,7 @@ import java.util.Calendar;
 
 
 public class InfoDebt extends Activity {
+    View view = null;
     public void onCreate(Bundle save){
         super.onCreate(save);
         setContentView(R.layout.credit_info);
@@ -45,6 +46,8 @@ public class InfoDebt extends Activity {
         tvDebt.setText(new DecimalFormat("###,###,###,###").format(Double.valueOf(AppData.DEBT)));
         tvDateDay.setText(String.valueOf(calendar.get(Calendar.DATE)));
         etPayment.setText(new DecimalFormat("###,###,###,###.00").format(Double.valueOf(AppData.PAYMENT)));
+
+        etPayment.selectAll();
 
         etPayment.addTextChangedListener(inControlFieldAddPayment);
 
@@ -74,6 +77,68 @@ public class InfoDebt extends Activity {
                 startActivity(new Intent(getBaseContext(), ListPayment.class));
             }
         });
+
+        new AsyncTask<Void, View, Void>() {
+            LinearLayout layout = (LinearLayout)findViewById(R.id.llPaymentInfo);
+            @Override
+            protected Void doInBackground(Void... voids) {
+                LayoutInflater inflater = (LayoutInflater)getSystemService(getBaseContext().LAYOUT_INFLATER_SERVICE);
+                WorkDB workDB = new WorkDB(getBaseContext());
+                Cursor cursorInPayment = workDB.readValueFromDataBase("SELECT " +
+                        DebtCalcDB.FIELD_PAYMENT_PAYMENTS +", "+
+                        DebtCalcDB.FIELD_DEBT_PAYMENTS +", "+
+                        DebtCalcDB.FIELD_PERCENT_PAYMENTS +", "+
+                        DebtCalcDB.FIELD_SUM_PAYMENTS +
+                        " FROM " + DebtCalcDB.TABLE_NAME_PAYMENTS +
+                        " WHERE (" + DebtCalcDB.FIELD_PAID_PAYMENTS + " = '1'" +
+                        " AND " + DebtCalcDB.FIELD_ID_DEBT_PAYMENTS + " = '" + AppData.ID_DEBT +"')");
+                int numPayment = 0;
+                while (cursorInPayment.moveToNext()){
+                    numPayment++;addRecord(inflater,
+                            layout,
+                            numPayment,
+                            cursorInPayment.getDouble(cursorInPayment.getColumnIndex(DebtCalcDB.FIELD_PAYMENT_PAYMENTS)),
+                            cursorInPayment.getDouble(cursorInPayment.getColumnIndex(DebtCalcDB.FIELD_DEBT_PAYMENTS)),
+                            cursorInPayment.getDouble(cursorInPayment.getColumnIndex(DebtCalcDB.FIELD_PERCENT_PAYMENTS)),
+                            cursorInPayment.getDouble(cursorInPayment.getColumnIndex(DebtCalcDB.FIELD_SUM_PAYMENTS)));
+                    publishProgress(view);
+                }
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+            @Override
+            protected void onProgressUpdate(View... values) {
+                super.onProgressUpdate(values);
+                layout.addView(values[0]);
+            }
+
+            protected void onPostExecute(View view){
+                if (view != null)
+                    layout.addView(view);
+            }
+        }.execute();
+    }
+
+    public View addRecord(LayoutInflater inflater,
+                          LinearLayout layout,
+                          int numPayment,
+                          Double payment,
+                          Double paymentDebt,
+                          Double paymentPercent,
+                          Double feePayment){
+        view = inflater.inflate(R.layout.payment_info_record, layout, false);
+
+        TextView tvNumPayment = (TextView)view.findViewById(R.id.tvNumPaymentRecord);
+        TextView tvPayment = (TextView)view.findViewById(R.id.tvPaymentRecord);
+        TextView tvDebt = (TextView)view.findViewById(R.id.tvDebtRecord);
+        TextView tvPercent = (TextView)view.findViewById(R.id.tvPercentRecord);
+        TextView tvFeePayment = (TextView)view.findViewById(R.id.tvSumPaymentRecord);
+
+        tvNumPayment.setText(String.valueOf(numPayment));
+        tvPayment.setText(new DecimalFormat("###,###,###,###,###.00").format(payment));
+        tvDebt.setText(new DecimalFormat("###,###,###,###,###.00").format(paymentDebt));
+        tvPercent.setText(new DecimalFormat("###,###,###,###,###.00").format(paymentPercent));
+        tvFeePayment.setText(new DecimalFormat("###,###,###,###,###.00").format(feePayment));
+        return view;
     }
 
     private String formatValue(String addPayment){
