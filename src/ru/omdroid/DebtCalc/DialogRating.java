d940d0a044ced2f5a8fa8212897b9b984f55cdf9
Bolid <dmitriy.grigorev@gmail.com>
2013-11-07 19:29:59 +0400

    Исправлены ошибка последнего числа месяца. Сделан диалог оценки

---
+++ src/ru/omdroid/DebtCalc/DialogRating.java
@@ -0,0 +1,43 @@
+package ru.omdroid.DebtCalc;
+
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import ru.omdroid.DebtCalc.DB.DebtCalcDB;
+import ru.omdroid.DebtCalc.DB.WorkDB;
+
+public class DialogRating extends DialogFragment implements View.OnClickListener {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        getDialog().setTitle(getResources().getString(R.string.app_name));
+        View view = inflater.inflate(R.layout.dialog_rating, null);
+        view.findViewById(R.id.butRatYes).setOnClickListener(this);
+        view.findViewById(R.id.butRatNo).setOnClickListener(this);
+        view.findViewById(R.id.butRatYesFix).setOnClickListener(this);
+        return view;
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.butRatYes:
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("market://details?id=ru.omdroid.DebtCalc"));
+                startActivity(intent);
+                dismiss();
+                break;
+            case R.id.butRatNo:
+                dismiss();
+                break;
+            case R.id.butRatYesFix:
+                WorkDB workDB = new WorkDB(getActivity().getBaseContext());
+                workDB.updateData("UPDATE " + DebtCalcDB.TABLE_SETTING + " SET " + DebtCalcDB.F_RATING_SHOW_SET + " = '1'");
+                workDB.disconnectDataBase();
+                dismiss();
+                break;
+        }
+    }
+}
