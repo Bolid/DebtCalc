6b6af33393d1f67619cbf3e59da356e0c8095ecf
Bolid <dmitriy.grigorev@gmail.com>
2014-01-27 23:33:12 +0400

    empty

--- src/ru/omdroid/DebtCalc/Widget/WidgetProvider.java
+++ src/ru/omdroid/DebtCalc/Widget/WidgetProvider.java
@@ -18,11 +18,22 @@ public class WidgetProvider extends AppWidgetProvider{
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
-        RemoteViews rView = new RemoteViews(context.getPackageName(), R.layout.widget);
+        /*RemoteViews rView = new RemoteViews(context.getPackageName(), R.layout.widget);
         Intent intent = new Intent(context, WidgetService.class);
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds);
         rView.setRemoteAdapter(R.id.lViewInWidget, intent);
-        appWidgetManager.updateAppWidget(appWidgetIds, rView);
+        appWidgetManager.updateAppWidget(appWidgetIds, rView);*/
+        for (int appWidgetId : appWidgetIds) {
+            onUpdateWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    public void onUpdateWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId){
+        RemoteViews rView = new RemoteViews(context.getPackageName(), R.layout.widget);
+        Intent intent = new Intent(context, WidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        rView.setRemoteAdapter(R.id.lViewInWidget, intent);
+        appWidgetManager.updateAppWidget(appWidgetId, rView);
     }
 
     @Override
